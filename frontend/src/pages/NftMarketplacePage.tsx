207e461182fd8aaafe989cd4b5e3c8a4a690ffa6
Andre <andreafragnelli@gmail.com>
2024-10-17 15:45:18 +0200

    Created Market Page for see NFTs from useGetNftsForSaleWithDetails hook

--- frontend/src/pages/NftMarketplacePage.tsx
+++ frontend/src/pages/NftMarketplacePage.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import NftSaleList from "../components/NftSaleList";
+import useGetNftsForSaleWithDetails from "../hooks/useGetNftsForSaleWithDetails";
+import loading from "../assets/imgs/loading.gif";
 
 const NftMarketplacePage = () => {
+  const { nftsForSaleWithDetails, isPending, error, refetch } =
+    useGetNftsForSaleWithDetails();
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  if (isPending)
+    return (
+      <div className="LoadingIcon">
+        <img src={loading} alt="loading..." />
+      </div>
+    );
+  if (error) return <div>Error in loading NFTs: {error.message}</div>;
+  if (!nftsForSaleWithDetails) return <div>No NFT available</div>;
+
   return (
     <>
       <Navbar />
+      <NftSaleList nfts={nftsForSaleWithDetails} />
     </>
   );
 };
