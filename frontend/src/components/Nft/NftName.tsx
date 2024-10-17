b7530c7f601091ed1bfe66a4ba35ac7c95c40f5d
Andre <andreafragnelli@gmail.com>
2024-10-17 16:31:58 +0200

    name fix

--- frontend/src/components/Nft/NftName.tsx
+++ frontend/src/components/Nft/NftName.tsx
@@ -1,14 +1,9 @@
 type NftNameProps = {
   name: string;
-  tokenId: bigint;
 };
 
-const NftName: React.FC<NftNameProps> = ({ name, tokenId }) => {
-  return (
-    <h2>
-      {name} (ID: {`Token ID: ${tokenId}`})
-    </h2>
-  );
+const NftName: React.FC<NftNameProps> = ({ name }) => {
+  return <h2>{name}</h2>;
 };
 
 export default NftName;
