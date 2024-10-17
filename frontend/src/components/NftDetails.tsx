57f58a157b1802ecde847f39cd28998a177a30af
Andre <andreafragnelli@gmail.com>
2024-10-17 18:22:54 +0200

    Added Purchase Nft Button and completed NftDetails page

--- frontend/src/components/NftDetails.tsx
+++ frontend/src/components/NftDetails.tsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+import PurchaseButton from "./PurchaseButton";
 
 type NftDetailsProps = {
   details: [bigint, string, bigint];
@@ -34,17 +35,22 @@ const NftDetails: React.FC<NftDetailsProps> = ({ details }) => {
 
   return (
     <>
-      <img
-        src={httpImageUrl}
-        alt="NFT Image"
-        width="512"
-        className="rounded-xl mb-3"
-      />
-      <p>
-        {" "}
-        {metadata?.name} ({`ID: ${tokenId}`}){" "}
-      </p>
-      <p>Price: {ethers.formatEther(price)} ETH</p>
+      <div className="flex justify-center">
+        <div className="flex justify-around items-center  w-[1100px] mt-5 ">
+          <img
+            src={httpImageUrl}
+            alt="NFT Image"
+            width="512"
+            className="rounded-xl"
+          />
+          <div className="flex flex-col justify-center items-start ml-5">
+            <p className=" text-4xl mb-10"> {metadata?.name}</p>
+            <p className="mb-5">{metadata?.description}</p>
+            <p className=" text-4xl ">Price: {ethers.formatEther(price)} ETH</p>
+            <PurchaseButton tokenId={tokenId} price={price} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
