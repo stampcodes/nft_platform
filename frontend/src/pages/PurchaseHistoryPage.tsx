5b681d7f454205ace0115911e95f68d6d29c3f1d
Andre <andreafragnelli@gmail.com>
2024-10-18 18:37:10 +0200

    Added EventList component with hook

--- frontend/src/pages/PurchaseHistoryPage.tsx
+++ frontend/src/pages/PurchaseHistoryPage.tsx
@@ -1,9 +1,13 @@
+import EventList from "../components/events/EventList";
 import Navbar from "../components/Navbar";
+import useNftPurchasedEvent from "../hooks/useNftPurchasedEvent";
 
 const PurchaseHistoryPage = () => {
+  const { events } = useNftPurchasedEvent();
   return (
     <>
       <Navbar />
+      <EventList events={events} />
     </>
   );
 };
