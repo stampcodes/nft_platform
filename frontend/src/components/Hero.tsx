7623d23aa7e2f097e63d98ac9dc8af125e51c730
Andre <andreafragnelli@gmail.com>
2024-10-19 13:35:43 +0200

    Added Hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="bgLab flex flex-col justify-start items-center min-h-screen">
+        <div className="w-full text-center">
+          <h2 className="text-white text-4xl bg-[#2c2c2c] p-5">
+            Madness is the Spark of Innovation - Join the Lab!
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
