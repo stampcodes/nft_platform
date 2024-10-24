9907ae2a328b2231ec5eb9b2c3e2ce0a4f8fb3ef
Andre <andreafragnelli@gmail.com>
2024-10-24 16:26:52 +0200

    Feat: added collection showcase card with CTA 'Step Into the Lab!' on homepage
    - Implemented a new section on the homepage displaying NFT collection cards.
    - Added a call-to-action button with the text 'Step Into the Lab!' to invite users to explore the collection.
    - Enhanced visual engagement by integrating the collection within the lab-themed design.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import heroImg from "../assets/imgs/hero.png";
+
 const Hero = () => {
+  const navigate = useNavigate();
+  const navigateToMarket = () => {
+    navigate("/market");
+  };
+
   return (
     <>
       <div className="bgLab flex flex-col justify-start items-center min-h-screen">
@@ -6,6 +14,12 @@ const Hero = () => {
           <h2 className="text-white text-4xl bg-[#2c2c2c] p-5 VT323">
             Madness is the Spark of Innovation - Join the Lab!
           </h2>
+        </div>
+        <div className="flex flex-col justify-center items-center p-5 w-[95%] md:w-[80%] lg:w-[65%] xl:w-[60%] 2xl:w-[55%] rounded-xl bg-[#00274d] mt-5">
+          <img src={heroImg} alt="NFT Collection" className="rounded-xl mb-5" />{" "}
+          <button className="customButton" onClick={navigateToMarket}>
+            Step Into the Lab!
+          </button>
         </div>
       </div>
     </>
