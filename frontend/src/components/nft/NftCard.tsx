b1224fe92fee47424b845985e2f85e76e203541a
Andre <andreafragnelli@gmail.com>
2024-10-24 14:42:50 +0200

    Feat: added loading indicators to NftCard and NftDetails components
    - Implemented loading state in both NftCard.tsx and NftDetails.tsx to show a loading icon while fetching metadata.
    - Added safety checks with 'metadata &&' to ensure content is rendered only after metadata is fully loaded.
    - Improved user experience by providing visual feedback during data fetching.

--- frontend/src/components/nft/NftCard.tsx
+++ frontend/src/components/nft/NftCard.tsx
@@ -3,6 +3,7 @@ import NftName from "./NftName";
 import NftImage from "./NftImage";
 import NftPrice from "./NftPrice";
 import { useNavigate } from "react-router-dom";
+import loading from "../../assets/imgs/loading.gif";
 
 type NftCardProps = {
   tokenId: bigint;
@@ -16,11 +17,13 @@ interface NftMetadata {
 
 const NftCard: React.FC<NftCardProps> = ({ tokenId, uri, price }) => {
   const [metadata, setMetadata] = useState<NftMetadata | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMetadata = async () => {
       try {
+        setIsLoading(true);
         const response = await fetch(
           uri.replace("ipfs://", "https://dweb.link/ipfs/")
         );
@@ -31,6 +34,8 @@ const NftCard: React.FC<NftCardProps> = ({ tokenId, uri, price }) => {
         setMetadata(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -46,12 +51,18 @@ const NftCard: React.FC<NftCardProps> = ({ tokenId, uri, price }) => {
       onClick={handleCardClick}
       className="border-none p-3 m-3 rounded-xl bg-[#00274d] card "
     >
-      {metadata && (
-        <>
-          <NftImage imageUrl={metadata.image} />
-          <NftName name={metadata.name} />
-          <NftPrice price={price} />
-        </>
+      {isLoading ? (
+        <div className="LoadingIcon ">
+          <img className="rounded-xl" src={loading} alt="loading..." />
+        </div>
+      ) : (
+        metadata && (
+          <>
+            <NftImage imageUrl={metadata.image} />
+            <NftName name={metadata.name} />
+            <NftPrice price={price} />
+          </>
+        )
       )}
     </div>
   );
--- frontend/src/components/nft/NftDetails.tsx
+++ frontend/src/components/nft/NftDetails.tsx
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import PurchaseButton from "./PurchaseButton";
+import loading from "../../assets/imgs/loading.gif";
 
 type NftDetailsProps = {
   details: [bigint, string, bigint];
@@ -9,10 +10,12 @@ type NftDetailsProps = {
 const NftDetails: React.FC<NftDetailsProps> = ({ details }) => {
   const [tokenId, uri, price] = details;
   const [metadata, setMetadata] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchMetadata = async () => {
       try {
+        setIsLoading(true);
         const response = await fetch(
           uri.replace("ipfs://", "https://dweb.link/ipfs/")
         );
@@ -21,6 +24,8 @@ const NftDetails: React.FC<NftDetailsProps> = ({ details }) => {
         setMetadata(data);
       } catch (error) {
         console.error("Errore nel recupero dei metadati:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -40,26 +45,34 @@ const NftDetails: React.FC<NftDetailsProps> = ({ details }) => {
         </h2>
         <div className="flex justify-center items-start">
           <div className="flex flex-col md:flex-row justify-center items-center p-5 w-[95%] md:w-[80%] lg:w-[65%] xl:w-[60%] 2xl:w-[55%] rounded-xl bg-[#00274d] mt-5">
-            <div className="flex flex-col md:flex-row justify-around items-center w-full md:w-[700px] lg:w-[900px] xl:w-[1000px] 2xl:w-[1100px]">
-              <img
-                src={httpImageUrl}
-                alt="NFT Image"
-                className="rounded-xl w-full sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[512px]"
-              />
-              <div className="flex flex-col justify-center items-start mt-5 md:mt-0 md:ml-5 w-full md:w-auto">
-                <p className="text-2xl sm:text-3xl md:text-4xl mb-4 text-white">
-                  {metadata?.name}
-                </p>
-                <p className="mb-5 text-white">{metadata?.description}</p>
-                <p className="text-2xl sm:text-3xl md:text-4xl text-white">
-                  Price:{" "}
-                  <span className="text-[#00bfff]">
-                    {ethers.formatEther(price)} ETH
-                  </span>
-                </p>
-                <PurchaseButton tokenId={tokenId} price={price} />
+            {isLoading ? (
+              <div className="LoadingIcon">
+                <img src={loading} alt="Loading..." className="rounded-xl" />
               </div>
-            </div>
+            ) : (
+              metadata && (
+                <div className="flex flex-col md:flex-row justify-around items-center w-full md:w-[700px] lg:w-[900px] xl:w-[1000px] 2xl:w-[1100px]">
+                  <img
+                    src={httpImageUrl}
+                    alt="NFT Image"
+                    className="rounded-xl w-full sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[512px]"
+                  />
+                  <div className="flex flex-col justify-center items-start mt-5 md:mt-0 md:ml-5 w-full md:w-auto">
+                    <p className="text-2xl sm:text-3xl md:text-4xl mb-4 text-white">
+                      {metadata?.name}
+                    </p>
+                    <p className="mb-5 text-white">{metadata?.description}</p>
+                    <p className="text-2xl sm:text-3xl md:text-4xl text-white">
+                      Price:{" "}
+                      <span className="text-[#00bfff]">
+                        {ethers.formatEther(price)} ETH
+                      </span>
+                    </p>
+                    <PurchaseButton tokenId={tokenId} price={price} />
+                  </div>
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
