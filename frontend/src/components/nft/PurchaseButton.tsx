3e3f3493042527d389e5661725a8f2c73b46d624
Andre <andreafragnelli@gmail.com>
2024-10-24 15:36:37 +0200

    Feat: added temporary error message and improved transaction feedback in PurchaseButton
    - Implemented a temporary error message display that disappears after 1.5 seconds.
    - Enhanced user experience by showing clearer feedback during transaction processing and confirmation.
    - Used isConnected to ensure proper wallet connection before initiating the purchase process.

--- frontend/src/components/nft/PurchaseButton.tsx
+++ frontend/src/components/nft/PurchaseButton.tsx
@@ -1,6 +1,7 @@
 import usePurchaseNft from "../../hooks/usePurchaseNft";
 import { useAccount } from "wagmi";
 import { useAppKit } from "@reown/appkit/react";
+import { useEffect, useState } from "react";
 
 type PurchaseButtonProps = {
   tokenId: bigint;
@@ -8,18 +9,29 @@ type PurchaseButtonProps = {
 };
 
 const PurchaseButton: React.FC<PurchaseButtonProps> = ({ tokenId, price }) => {
-  const { address } = useAccount();
+  const { isConnected } = useAccount();
   const { open } = useAppKit();
+  const [showError, setShowError] = useState<boolean>(false);
 
   const { handlePurchaseNft, error, isPending, isConfirming, isConfirmed } =
     usePurchaseNft(tokenId, price);
 
   const handleClick = async (e: any) => {
-    if (!address) {
+    if (!isConnected) {
       await open();
     }
     handlePurchaseNft(e);
   };
+
+  useEffect(() => {
+    if (error) {
+      setShowError(true);
+      const timer = setTimeout(() => {
+        setShowError(false);
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   return (
     <>
@@ -29,18 +41,19 @@ const PurchaseButton: React.FC<PurchaseButtonProps> = ({ tokenId, price }) => {
           className="customButton mt-5"
           onClick={handleClick}
         >
-          {isPending ? "Confirming..." : "Purchase NFT"}
+          {isPending ? "Transaction in Progress..." : "Purchase NFT"}
         </button>
       )}
-
-      {isConfirmed && (
-        <p className="successfulPayment">Thank you for your purchase.</p>
-      )}
-      {error && (
+      {showError && (
         <p className="failedPayment">Payment failed. Please try again.</p>
       )}
       {isConfirming && (
-        <div className="waitingPayment">Waiting for confirmation...</div>
+        <div className="waitingPayment">
+          Transaction submitted. Waiting for confirmation...
+        </div>
+      )}
+      {isConfirmed && (
+        <p className="successfulPayment">Thank you for your purchase.</p>
       )}
     </>
   );
